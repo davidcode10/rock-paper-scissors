45226cdf933a7bd93deae926e5ee59beae9adb90
davidcode10 <davidedson2030@outlook.com>
2022-02-25 12:50:46 +0100

    other game improvements implemented

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,49 +1,51 @@
 import random
+import game_options
 
 user_wins = 0
 computer_wins = 0
-
-options = ['rock', 'paper', 'scissors']
 
 while True:
     user_input = input('Type Rock/Paper/Scissors or Q to quit: ').lower()
     if user_input == 'q':
         break
 
-    if user_input not in options:
+    if user_input not in game_options.options:
         continue
 
     random_number = random.randint(0, 2)
 
-    computer_pick = options[random_number]
+    computer_pick = game_options.options[random_number]
     print('Computer picked', computer_pick + '.')
 
     if user_input == 'rock' and computer_pick == 'scissors':
         print('You won!')
         user_wins += 1
+        print('You won', user_wins, 'times.')
+        print('The computer won', computer_wins, 'times')
         
     elif user_input == 'paper' and computer_pick == 'rock':
         print('You won!')
         user_wins += 1
+        print('You won', user_wins, 'times.')
+        print('The computer won', computer_wins, 'times')
         
     elif user_input == 'scissors' and computer_pick == 'paper':
         print('You won!')
         user_wins += 1
+        print('You won', user_wins, 'times.')
+        print('The computer won', computer_wins, 'times')
+
     elif user_input == computer_pick:
         print('Draw!')
+        print('You won', user_wins, 'times.')
+        print('The computer won', computer_wins, 'times')
 
     else:
         print('You lost!')
         computer_wins += 1
-
-    if user_wins >= 1:
-        print('You won', user_wins, 'time')
-    else:
         print('You won', user_wins, 'times.')
+        print('The computer won', computer_wins, 'times')
 
-    if computer_wins >= 1:
-        print('The computer won', computer_wins, 'time')
-    else:
-        print('The computer won', computer_wins, 'times')    
-
+print('You won', user_wins, 'times.')
+print('The computer won', computer_wins, 'times')    
 print('Goodbye!')
